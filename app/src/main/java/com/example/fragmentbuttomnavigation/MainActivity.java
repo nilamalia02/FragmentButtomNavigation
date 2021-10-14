da920f6ff34623120b314fad880d8e129bb3d07a
nilamalia02 <nilaa3794@gmail.com>
2021-10-14 16:22:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragmentbuttomnavigation/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.fragmentbuttomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+//untuk menjadikan chat menjadi menu utama
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChatFragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.nav_chats:
+                        selectedFragment = new ChatFragment();
+                        break;
+                    case R.id.nav_status:
+                        selectedFragment = new StatusFragment();
+                        break;
+                    case R.id.nav_calls:
+                        selectedFragment = new CallsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
